286bc894e55337e362dfc3b5ccc0163c0373861f
kaTCooop <49354314+kaTCooop@users.noreply.github.com>
2025-04-27 22:41:48 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from models import Base
 from test_objects import create_default_objects
 
 
-def create_db_and_tables() -> None:
+def create_db_and_tables():
     db_url = os.environ.get('DB_URL')
     engine = create_engine(db_url)
     Base.metadata.create_all(engine)
